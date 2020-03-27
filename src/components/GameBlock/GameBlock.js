77a61dbc5cdcecf7af2b68f1f74959aadcab5180
kyrylokravchuk <kirill.kravchyk@gmail.com>
2020-03-27 10:45:17 +0200

    added flag styles and logic

--- src/components/GameBlock/GameBlock.js
+++ src/components/GameBlock/GameBlock.js
@@ -3,17 +3,18 @@ import classes from './GameBlock.module.css';
 
 const GameBlock = ({x, y, type, isBombsShowed, onBombClick, onSetFlagClick, getCloseBombsAmount}) => {
 	const isBomb = type === 'b';
-	const icon = isBomb ? <div className={classes.Bomb} /> : null;
 	const [amount, setAmount] = useState(0);
 	const [clicked, setClicked] = useState(false);
+	const [isFlagSet, setIsFlagSet] = useState(false);
 	const [isBoomed, setIsBoomed] = useState(false);
 	
 	useEffect(() => {
 		setAmount(getCloseBombsAmount(x, y));
 	}, [type, getCloseBombsAmount]);
 	
-	const onBlockClick = (event) => {
+	const onBlockClick = event => {
 		if (isBombsShowed) return;
+		if (isFlagSet) return;
 		
 		setClicked(true);
 		event.target.style.backgroundColor = '#bfbfbf';
@@ -24,11 +25,19 @@ const GameBlock = ({x, y, type, isBombsShowed, onBombClick, onSetFlagClick, getC
 		}
 	};
 	
+	const onContextBlockClick = event => {
+		setIsFlagSet(!isFlagSet);
+		onSetFlagClick(event);
+	};
+	
+	const bombIcon = isBomb ? <div className={classes.Bomb} /> : null;
+	const flagIcon = isFlagSet ? <div className={classes.Flag} /> : null;
 	const amountParagraph = <span>{amount}</span>;
 	
 	return (
-		<section className={classes.GameBlock} onClick={onBlockClick} onContextMenu={onSetFlagClick}>
-			{isBombsShowed && icon}
+		<section className={classes.GameBlock} onClick={onBlockClick} onContextMenu={onContextBlockClick}>
+			{isBombsShowed && bombIcon}
+			{isFlagSet && flagIcon}
 			{clicked && !isBoomed && amount !== 0 && amountParagraph}
 		</section>
 	)
